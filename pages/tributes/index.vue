2b910e4fe268f4bb90b8f542a208d5c37d3bc0e5
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-07-06 14:48:55 +0100

    updated

--- pages/tributes/index.vue
+++ pages/tributes/index.vue
@@ -1347,6 +1347,75 @@
         </div>
         <!-- divider end -->
         <img
+          :src="minlaw"
+          alt="oge's condolence"
+          class="rounded-md transform transition duration-500 hover:scale-125"
+        />
+        <!-- divider start -->
+        <div
+          class="flex items-center justify-center py-8 space-x-4 text-red-500"
+        >
+          <div class="bg-red-500 w-2 h-2 rounded-full"></div>
+          <svg class="w-4 h-4" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M3,13A9,9 0 0,0 12,22A9,9 0 0,0 3,13M12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,22M18,3V8A6,6 0 0,1 12,14A6,6 0 0,1 6,8V3C6.74,3 7.47,3.12 8.16,3.39C8.71,3.62 9.2,3.96 9.61,4.39L12,2L14.39,4.39C14.8,3.96 15.29,3.62 15.84,3.39C16.53,3.12 17.26,3 18,3Z"
+            />
+          </svg>
+          <svg class="w-6 h-6" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M3,13A9,9 0 0,0 12,22A9,9 0 0,0 3,13M12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,22M18,3V8A6,6 0 0,1 12,14A6,6 0 0,1 6,8V3C6.74,3 7.47,3.12 8.16,3.39C8.71,3.62 9.2,3.96 9.61,4.39L12,2L14.39,4.39C14.8,3.96 15.29,3.62 15.84,3.39C16.53,3.12 17.26,3 18,3Z"
+            />
+          </svg>
+          <span class="flex items-center justify-center w-6 h-6">
+            <span
+              class="
+                animate-ping
+                absolute
+                inline-flex
+                h-8
+                w-8
+                rounded-full
+                bg-red-500
+                opacity-75
+              "
+            ></span>
+            <span
+              class="
+                relative
+                flex
+                items-center
+                justify-center
+                rounded-full
+                h-12
+                w-12
+              "
+            >
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M3,13A9,9 0 0,0 12,22A9,9 0 0,0 3,13M12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,22M18,3V8A6,6 0 0,1 12,14A6,6 0 0,1 6,8V3C6.74,3 7.47,3.12 8.16,3.39C8.71,3.62 9.2,3.96 9.61,4.39L12,2L14.39,4.39C14.8,3.96 15.29,3.62 15.84,3.39C16.53,3.12 17.26,3 18,3Z"
+                />
+              </svg>
+            </span>
+          </span>
+          <svg class="w-6 h-6" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M3,13A9,9 0 0,0 12,22A9,9 0 0,0 3,13M12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,22M18,3V8A6,6 0 0,1 12,14A6,6 0 0,1 6,8V3C6.74,3 7.47,3.12 8.16,3.39C8.71,3.62 9.2,3.96 9.61,4.39L12,2L14.39,4.39C14.8,3.96 15.29,3.62 15.84,3.39C16.53,3.12 17.26,3 18,3Z"
+            />
+          </svg>
+          <svg class="w-4 h-4" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M3,13A9,9 0 0,0 12,22A9,9 0 0,0 3,13M12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,22M18,3V8A6,6 0 0,1 12,14A6,6 0 0,1 6,8V3C6.74,3 7.47,3.12 8.16,3.39C8.71,3.62 9.2,3.96 9.61,4.39L12,2L14.39,4.39C14.8,3.96 15.29,3.62 15.84,3.39C16.53,3.12 17.26,3 18,3Z"
+            />
+          </svg>
+          <div class="bg-red-500 w-2 h-2 rounded-full"></div>
+        </div>
+        <!-- divider end -->
+        <img
           :src="ezigbo"
           alt="oge's condolence"
           class="rounded-md transform transition duration-500 hover:scale-125"
@@ -1716,6 +1785,9 @@ export default {
     window.scrollTo(0, 0);
   },
   asyncData({ $cloudinary }) {
+    const minlaw = $cloudinary.image.url("goodbye/minlaw", {
+      quality: "auto",
+    });
     const ezigbo = $cloudinary.image.url("goodbye/ezigbo", {
       quality: "auto",
     });
@@ -1728,7 +1800,7 @@ export default {
     const stAnns = $cloudinary.image.url("goodbye/st_Anns", {
       quality: "auto",
     });
-    return { ezigbo, oge, ugonna, stAnns };
+    return { minlaw, ezigbo, oge, ugonna, stAnns };
   },
 };
 </script>
